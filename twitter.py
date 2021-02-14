72931d19c4803eb1a5d054a674d2ed2e70dff897
Wai Han <wh.kankan13@gmail.com>
2021-02-15 00:50:51 +0630

    :cloud: Like and retweet from the specified user account

--- twitter.py
+++ twitter.py
@@ -1,14 +1,61 @@
+#!/usr/bin/env python3
+
 import time
 import tweepy
+import tkinter as tk
 
 auth = tweepy.OAuthHandler("bbI8Zu5WBmAfyqvAKFkMDQbAx", "YCwI8S6bY3jrsfke3MAcBSKO1Dr8OFengEroLvvMNkMnPD5JJX")
+
 auth.set_access_token("1356590402992623619-4DMgFemZEAbLOx52rF7h8CTjVWhw0q",
                       "j6291l2btbERyZxaa7Q8SwiooRRtr4rJ20OoA44kA9dTJ")
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-user = api.get_user('yunwaddylm')
-print(user.screen_name)
-print(user.followers_count)
-for friend in user.friends():
-    print(friend.screen_name)
+username = input("Enter the screen name of a person you want to retweet the timeline from: ")
+number_of_tweets = input("Enter the number of tweets: ")
+
+print("Username: " + username)
+print("No.of tweets: " + number_of_tweets + "\n")
+
+print(" _________________________")
+print("|                         |")
+print("| Starting Retweet + Like |")
+print("|                         |")
+print(" ------------------------- ")
+
+user = api.get_user(username)
+timeline = api.user_timeline(username, count=number_of_tweets)
+num = 1
+
+for tweet in timeline:
+
+    try:
+        print("\n\nTweet: " + str(num))
+        print("______________")
+        print(tweet.text)
+        print(tweet.user.screen_name)
+        print("______________")
+        num += 1
+
+        if not tweet.favorited:
+            tweet.favorite()
+            print("Like tweet")
+
+        else:
+            print("Post already liked")
+
+        if not tweet.retweeted:
+            tweet.retweet()
+            print("Retweet")
+
+        else:
+            print("Post already retweet")
+            
+        time.sleep(5)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+        
+    except StopIteration:
+        break
+
